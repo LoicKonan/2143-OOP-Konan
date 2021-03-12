9fa27972ee07f1f03fb86fe813fd4f58371d9811
Loic <konanloic10@gmail.com>
2021-03-12 15:14:28 -0600

    README.md

--- Assignments/A08/main.cpp
+++ Assignments/A08/main.cpp
@@ -4,55 +4,12 @@
 
 using namespace std;
 
-/**
- * This class is a simple bare bones example showing use of a copy constructor
- * for the question on the exam.
- * 
- */
-class NumContainer
-{
-    int *array; // int pointer (dynamic memory)
-    int size;
-
-public:
-    ~NumContainer();
-    NumContainer()
-    {
-        size = 100;
-        array = new int[size];
-    }
-    NumContainer(int s)
-
-    {
-        size = s;
-        array = new int[size];
-    }
-    NumContainer(const NumContainer &other)
-    {
-        size = other.size;
-        array = new int[size];
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = other.array[i];
-        }
-    }
-};
-
-/**
- *  Destrurctor ~NumContainer()
- */
-NumContainer::~NumContainer() {}
-
-/**
- * This is an implementation of our single color rgb class. 
- * 
- */
 class RgbColor
 {
 protected:
-    int r; //int 0-255 red
-    int g; //int 0-255 green
-    int b; //int 0-255 blue
+    int r;                  // int 0-255 red
+    int g;                  // int 0-255 green
+    int b;                  // int 0-255 blue
 
 
 public:
@@ -61,23 +18,21 @@ public:
     RgbColor(int);           // overloaded 1
     RgbColor(int, int, int); // overloaded 2
 
-    void SetR(int); // setter red
-    void SetG(int); // setter green
-    void SetB(int); // setter blue
+    void SetR(int);          // setter red
+    void SetG(int);          // setter green
+    void SetB(int);          // setter blue
 
-    int GetR(); // getter red
-    int GetG(); // getter green
-    int GetB(); // getter blue
+    int GetR();              // getter red
+    int GetG();              // getter green
+    int GetB();              // getter blue
 
-    void GrayScale(); // averages colors
+    void GrayScale();        // averages colors
 
     void AddColor(RgbColor rgbcolor);
 
-    // print to stdout
-    friend ostream &operator<<(ostream &, const RgbColor &);
+    friend ostream &operator<<(ostream &, const RgbColor &); // print to stdout
 
-    // add (mix) two colors
-    RgbColor operator+(const RgbColor &);
+    RgbColor operator+(const RgbColor &);   // add (mix) two colors
 
     static vector<RgbColor> myColors;
 };
@@ -116,18 +71,6 @@ RgbColor::RgbColor(int _r, int _g, int _b)
     b = _b;
 }
 
-ostream &operator<<(ostream &os, const RgbColor &rhs)
-{
-    //system("Color 04");
-    os << "[" << rhs.r << "," << rhs.g << "," << rhs.b << "]";
-    return os;
-}
-
-
-
-vector<RgbColor> RgbColor::myColors;
-
-
 void RgbColor:: SetR(int red)
 {
     r = red;
@@ -162,6 +105,18 @@ int RgbColor::GetB()
 {
 	myColors.push_back(rgbcolor);
 }
+
+
+
+ostream &operator<<(ostream &os, const RgbColor &rhs)
+{
+    // system("Color 04");
+    os << "[" << rhs.r << "," << rhs.g << "," << rhs.b << "]";
+    return os;
+}
+
+vector<RgbColor> RgbColor::myColors;
+
 
 int main()
 {
